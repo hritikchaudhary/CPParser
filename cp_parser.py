a9252da63c45a90a0b5e2c3aa5d6bc14568ea3ce
Hritik Chaudhary <33057454+hritikchaudhary@users.noreply.github.com>
2023-12-23 15:00:26 +0530

    codechef support

--- cp_parser.py
+++ cp_parser.py
@@ -6,36 +6,26 @@ import re
 import json
 from bs4 import BeautifulSoup
 
+# Constants
 LAST_URL = ''
-SUPPORTED_WEBSITES = ['codeforces.com']
 
-
-def is_supported_website(url):
-    for site in SUPPORTED_WEBSITES:
-        if site in url:
-            return True
-    return False
+# Load settings from the Sublime Text configuration file
 
 
 def load_settings():
     return sublime.load_settings('CPParser.sublime-settings')
 
-
-def get_parent_dir(settings):
-    parent_dir = settings.get('PARENT_DIR', '.')
-    return os.path.join(parent_dir)
+# Function to create a file based on given parameters
 
 
-def get_extension(settings):
-    return settings.get('EXTENSION', '')
-
-
-def create_file(parent_dir, title, extension, testcases):
+def create_file(parent_dir, title, extension, testcases, contest):
     try:
+        # Ensure the given directory exists or create it
         os.makedirs(parent_dir, mode=0o777, exist_ok=True)
-        filename = os.path.join(parent_dir, title + "_" + str(re.findall(r'\d+', LAST_URL)[
-                                0]) + str(re.findall(r'[A-Z]', LAST_URL)[0]) + '.' + extension)
+        filename = os.path.join(parent_dir, title + contest + '.' + extension)
         open(filename, "a").close()
+
+        # Write test cases to a separate file
         input_file = filename + ':tests'
         with open(input_file, 'w') as file:
             json.dump(testcases, file, indent=4)
@@ -45,6 +35,8 @@ def create_file(parent_dir, title, extension, testcases):
         print("Exception: ", e)
         sublime.error_message("Exception: File not created - " + str(e))
         return None
+
+# Parse content from Codeforces URL
 
 
 def parse_content_codeforces(url):
@@ -75,26 +67,87 @@ def parse_content_codeforces(url):
         sublime.error_message("Unable to parse: " + url)
         return None
 
+# Generate CodeChef API URL
+
+
+def generate_codechef_api_url(url):
+    try:
+        parts = url.split('/')
+        problem_index = parts.index('problems')
+        contest_code = parts[problem_index - 1]
+        problem_code = parts[problem_index + 1]
+        if (contest_code == 'www.codechef.com'):
+            contest_code = 'PRACTICE'
+        api_url = f"https://www.codechef.com/api/contests/{contest_code}/problems/{problem_code}"
+        return api_url
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        return None
+
+# Parse content from CodeChef URL
+
+
+def parse_content_codechef(url):
+    global LAST_URL
+    LAST_URL = url
+    try:
+        request = urllib.request.Request(
+            generate_codechef_api_url(url), headers={'User-Agent': 'Mozilla/5.0'})
+        response = urllib.request.urlopen(request)
+        page = response.read()
+
+        json_data = json.loads(page)
+        sample_test_cases = json_data.get(
+            "problemComponents", {}).get("sampleTestCases", [])
+
+        all_testcases = []
+        for test_case in sample_test_cases:
+            input_data = test_case.get("input")
+            all_testcases.append({
+                "test": input_data
+            })
+        title = json_data.get("problem_code")
+        return [title, all_testcases]
+    except Exception as e:
+        print(e)
+        sublime.error_message("Unable to parse: " + url)
+        return None
+
+# Main parse function handling URL parsing and file creation
+
 
 def parse(self, url):
     if url is None or url.strip() == '':
+        sublime.error_message("Please enter a URL")
         return
+    contest = ''
+    content = None
+    title = ''
+    testcases = []
+    parent_dir = ''
 
-    if not is_supported_website(url):
+    settings = load_settings()
+    extension = settings.get('EXTENSION', '')
+    if 'codeforces.com' in url:
+        content = parse_content_codeforces(url)
+        contest = "_" + str(re.findall(r'\d+', url)
+                            [0]) + str(re.findall(r'[A-Z]', url)[0])
+        title = content[0]
+        testcases = content[1]
+        parent_dir = os.path.join(settings.get('CODEFORCES_DIR', '.'))
+    elif 'codechef.com' in url:
+        content = parse_content_codechef(url)
+        title = content[0]
+        testcases = content[1]
+        parent_dir = os.path.join(settings.get('CODECHEF_DIR', '.'))
+    else:
         sublime.error_message("Unsupported website")
         return
 
-    content = parse_content_codeforces(url)
     if content is None:
         return
 
-    title = content[0]
-    testcases = content[1]
-    settings = load_settings()
-    parent_dir = get_parent_dir(settings)
-    extension = get_extension(settings)
-    file = create_file(parent_dir, title, extension, testcases)
-
+    file = create_file(parent_dir, title, extension, testcases, contest)
     if file is None:
         return
 
@@ -109,7 +162,6 @@ def parse(self, url):
             sublime.error_message("File not found " +
                                   snippets_file_name + " - " + str(e))
             return
-
     with open(file, 'w') as newfile:
         newfile.writelines(snippets_content)
     with open(file, 'r') as new_file:
